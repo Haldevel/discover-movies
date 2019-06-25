dfbdfdeb306eb481f9454d80066c1a7a070ca9b8
Susie <45341415+Haldevel@users.noreply.github.com>
2019-06-24 17:25:43 -0700

    add onChange to searchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
-
+//class component SearchBar
 class SearchBar extends React.Component {
+
+    //define state
+    state = { title: ''};
+
     render() {
         return (
-            <div className="row" style={{marginTop: '20px'}}>
+            <div className="row" style={{marginTop: '20px', marginLeft:'20px'}}>
                 <form className="col s10 m8">
                     <div className="row">
                         <div className="input-field col s10">
                             <i className="material-icons prefix">search</i>
-                            <input className="input-field" id="search" type="text"></input>
+                            <input className="input-field" id="search" type="text" value = {this.state.title} onChange={(e) => this.setState({ title: e.target.value})}></input>
                             <label for="search">Search for movies</label>
                         </div>
                         
